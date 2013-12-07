7f540c62f664c7fcf85971222d4af91a4f3cd35e
Chris Loy <chrisloyuk@gmail.com>
2013-12-07 21:11:45 +0000

    Added CSS and basic style

--- src/main/scala/net/escalatr/EscalatrServlet.scala
+++ src/main/scala/net/escalatr/EscalatrServlet.scala
@@ -3,32 +3,54 @@ package net.escalatr
 import org.scalatra._
 import scalate.ScalateSupport
 import net.escalatr.interpreter.Interpreter
+import scala.util.{Failure, Success, Try}
 
 class EscalatrServlet extends EscalatrStack {
 
   val interpret = new Interpreter
 
   get("/") {
-    <html>
-      <body>
-        <h1>escalatr</h1>
-        <form action="eval" id="usrform">
-          <input type="submit"/>
-          </form>
-        <textarea rows="4" cols="50" name="code" form="usrform">
-        </textarea>
-      </body>
-    </html>
+    template(
+      <textarea rows="10" cols="50" name="code" form="usrform"></textarea>
+      <form action="eval" id="usrform" method="post">
+        <div id="submit"><input type="submit" value="Do it!"/></div>
+      </form>
+    )
   }
 
-  get("/eval") {
+  post("/eval") {
     val code = params("code")
-    val result = interpret(code).toString
+    val result = Try(interpret(code).toString)
+    val (message, to) = result match {
+      case Success(output) => (output, "... compiled to ...")
+      case Failure(exception) => (exception.getMessage, "... didn't compile due to ...")
+    }
+    template(
+      <textarea rows="10" cols="50" name="code" form="usrform">{code}</textarea>
+      <form action="eval" id="usrform" method="post">
+        <div id="submit"><input type="submit" value="Do it!"/></div>
+      </form>
+      <p>{to}</p>
+      <div id="output"><p>{message}</p></div>
+    )
+  }
+
+  private val head =
+    <head>
+      <link rel="stylesheet" type="text/css" href="/css/styles.css"/>
+      <title>escalatr</title>
+    </head>
+
+  private def template(body: xml.NodeBuffer) =
     <html>
+      {head}
       <body>
-        <h1>{code} => </h1>
-        <h2>{result}</h2>
+        <h1><i>escalatr</i></h1>
+        <p id="strap">A fun place where learning about Scala can happen through your eyes and fingers</p>
+        <div id="content">
+          {body}
+          <div class="footer">About etc</div>
+        </div>
       </body>
     </html>
-  }
 }
