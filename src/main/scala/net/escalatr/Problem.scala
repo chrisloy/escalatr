aec1c7c1f7c71491ab716c0d62d138c46584c8c9
Chris Loy <chrisloyuk@gmail.com>
2013-12-09 22:44:20 +0000

    Basic problem flow added
    - 6 basic problems
    - Some style tweaks in CSS and page layout
    - Changed problem URLs to /problem/:id

--- src/main/scala/net/escalatr/Problem.scala
+++ src/main/scala/net/escalatr/Problem.scala
@@ -9,7 +9,17 @@ object Problem {
 
     """val x = 1 + 2
       |6 == x + ?
-    """.stripMargin
+    """.stripMargin,
+
+    "List(1, 2, ?) == 1 :: 2 :: 3 :: Nil",
+
+    "List(2, 4, 6) == (List(1, 2, 3) map (_ * ?))",
+
+    """def sq(x: Int) = x * x
+      |(List(1, 2, 3) map sq) == ?""".stripMargin,
+
+    """def countDown(x: Int): List[Int] = ?
+      |List(5, 4, 3, 2, 1) == countDown(5)""".stripMargin
   )
 
 }
